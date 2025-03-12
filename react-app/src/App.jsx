169076d9492d41743882ec032ff477f91227ea2b
Gagancreates <gaganp000999@gmail.com>
2025-03-13 00:13:37 +0530

    new

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,13 +1,30 @@
-import UserCard from "./components/UserCard";
-function App(){
-  return(
-  <div>
-    <h1>This was about day 3</h1>
-    <UserCard />
+// import UserCard from "./components/UserCard";
+// function App(){
+//   return(
+//   <div>
+//     <h1>This was about day 3</h1>
+//     <UserCard />
 
-  </div>
-  );
+//   </div>
+//   );
   
 
+// };
+// export default App;
+
+
+import { useState } from "react";
+function App(){
+  const [hey, update]=useState({count: 0})
+  return(
+    <div>
+
+    <h1>Count: {hey.count}</h1>
+    <button onClick={()=> { update({count: hey.count+1}) }}>Increase</button>
+    {hey.count>0 && (<button onClick={()=> { update({count: hey.count-1}) }}>Decrease</button>)}
+    </div>
+  );
+
 };
+
 export default App;
